a1e551279eeae4f1a0a0dbc1dbd3091328fc344d
amgwazo <amgwazo@hotmail.com>
2023-11-18 04:05:18 +0200

    secured the videos page with localstorage token check

--- frontend/netflix-front/src/pages/Videos.jsx
+++ frontend/netflix-front/src/pages/Videos.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
+import { useNavigate } from 'react-router-dom';
 
 const Videos = () => {
+ const isUserSignedIn = !!localStorage.getItem("token");
+ console.log(isUserSignedIn + " in Account Page");
+ const navigate = useNavigate();
+
+ // Redirect to login if user is not authenticated
+ if (!isUserSignedIn) {
+   navigate("/login");
+   return null; // Render nothing if redirecting
+ }
+
   return (
     <>
       <div className="d-flex flex-column">
